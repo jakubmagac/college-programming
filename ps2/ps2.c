d9b3e7c0d723c75f05aacc6c0c08fbeae663bf63
Jakub Magáč <jakub.magac@student.tuke.sk>
2019-11-01 18:53:32 +0100

    prve zadanie vylepsene

--- ps2/ps2.c
+++ ps2/ps2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*int stick_length, int human_weight, int car_weight -> float on 2 decimal places
-return distance where you have to put flucrum for lever */
-float lift_a_car(const int stick_length, const int human_weight, const int car_weight)
+/*
+*   float -> float
+*   round to 2 decimal places*/
+float round_decimal(float result)
 {
-    float result = (float)(stick_length * human_weight) / (float)(human_weight + car_weight);
     float helper = result * 1000;
     int reminder = (int)helper % 10;
     if(reminder >= 5)
@@ -17,9 +17,28 @@ float lift_a_car(const int stick_length, const int human_weight, const int car_w
     }
     result = (int)helper / 100.0;
     return result;
-} 
+}
+
+
+/*
+*   int, int, int -> float on 2 decimal places
+*   return distance where you have to put flucrum for lever */
+float lift_a_car(const int stick_length, const int human_weight, const int car_weight)
+{
+    float result = (float)(stick_length * human_weight) / (float)(human_weight + car_weight);
+    return round_decimal(result);
+}
+
+/*
+*   
+*/
+float unit_price(const float pack_price, const int rolls_count, const int pieces_count) 
+{
+
+}
 
 int main()
 {
+    
     return 0;
 }
