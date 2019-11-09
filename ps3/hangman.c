f0dccbafd1844b11e1b6e56e65d83060b9594604
Jakub Magáč <jakub.magac@student.tuke.sk>
2019-11-10 00:36:06 +0100

    prvy commit

--- ps3/hangman.c
+++ ps3/hangman.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include "hangman.h"
+#include <string.h>
 
 
 /*int get_word(char secret[]){
@@ -35,6 +36,84 @@
 
 int is_word_guessed(const char secret[], const char letters_guessed[])
 {
-    int size = sizeof(secret)/sizeof(secret[0]);
-    return size;
+    int size_secret = strlen(secret);
+    int size_guessed = strlen(letters_guessed);
+
+    char copy_secret[size_secret];
+    for(int i = 0; i < size_secret; i++) {
+        copy_secret[i] = secret[i];
+    }
+    
+    char copy_guessed[size_guessed];
+    for(int i = 0; i < size_guessed; i++) {
+        copy_guessed[i] = letters_guessed[i];
+    }
+
+    for(int i=0;i<size_secret;i++)
+    {
+        for(int s=0;s<size_guessed;s++)
+        {
+            if(copy_secret[i] == copy_guessed[s])
+            {
+                copy_secret[i] = '*';
+            }
+        }
+    }
+
+    for(int i=0;i<size_secret;i++)
+    {
+        if(copy_secret[i] != '*') return 0;
+    }
+    return 1;
+}
+
+void get_guessed_word(const char secret[], const char letters_guessed[], char guessed_word[])
+{
+    int size_secret = strlen(secret);
+    int size_guessed = strlen(letters_guessed);
+
+    for(int i=0;i<size_secret;i++)
+    {
+        guessed_word[i] = '_';
+    }
+
+    for(int s=0;s<size_guessed;s++)
+    {
+        for(int i=0;i<size_secret;i++)
+        {
+            if(secret[i] == letters_guessed[s])
+            {
+                guessed_word[i] = secret[i];
+            }
+        }
+    }
+}
+
+void get_available_letters(const char letters_guessed[], char available_letters[]) 
+{
+    int size_guessed = strlen(letters_guessed);
+    int size_available = strlen(available_letters);
+
+    for(int i=0;i<size_guessed;i++)
+    {
+        for(int s=0;s<size_available;s++)
+        {
+            if(letters_guessed[i] == available_letters[s])
+            {
+                available_letters[s] = '*';
+            }
+        }
+    }
+    for(int s=0;s<size_available;s++)
+    {
+        if(available_letters[s]=='*')
+        {
+            for(int i=s;i<size_available;i++){
+                available_letters[i] = available_letters[i+1];
+            }
+            if(available_letters[i+1] == '*') i--;
+
+
+        }
+    }
 }
--- ps3/main.c
+++ ps3/main.c
@@ -7,5 +7,19 @@ int main()
     printf("%d\n", is_word_guessed("secret", "aeiou"));
     // prints: 0
     printf("%d\n", is_word_guessed("hello", "aeihoul"));
+
+    char result[30];
+    get_guessed_word("container", "arpstxgoieyu", result);
+    // result = "_o_tai_er"
+
+    printf("%s", result);
+    printf("\n");
+
+    char vypis[] = "abcdefghijklmnopqrstuvwxyz";
+    get_available_letters("arpstxgoieyu", vypis);
+    printf("%s", vypis);
+    printf("\n");
+
+
     return 0;
 }
