b23724661cf791e3248b2f9a986d829b92ba2baa
Jakub Magáč <jakub.magac@student.tuke.sk>
2019-12-04 17:02:10 +0100

    asi jo

--- ps5/every_second.c
+++ ps5/every_second.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
     wFile = fopen("file.txt","w");
 
     if(argc != 2){
-        rFile = fopen(argv[1], "r");
+        rFile = fopen(argv[1], "w");
         fclose(rFile);
         return -1;
     }
