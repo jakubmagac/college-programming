a99609ac839a9572d13f17c704c7acc2b2b14a75
Jakub Magáč <jakub.magac@student.tuke.sk>
2019-12-12 19:21:56 +0100

    hra hotova

--- ps6/program.c
+++ ps6/program.c
@@ -6,19 +6,45 @@
 void init();
 void menu(int dificulty);
 void si_skoncil();
+void si_vyhral();
 void prvy_level(int dificulty);
 
-// e, m, h
 int main(int argc, char *argv[])
 {
-    int dificulty = 100000;
-    //if(argv[0] == 'e') dificulty = 100000;
-    //if(argv[0] == 'm') dificulty = 50000;
-    //if(argv[0] == 'h') dificulty = 10000;
+    int dificulty; 
+    if( argc == 2 ) {
+        switch (argv[1][0]){
+            case 'e':
+                dificulty = 100000;
+                break;
+
+            case 'm':
+                dificulty = 50000;
+                break;
+            
+            case 'h':
+                dificulty = 25000;
+                break;
+            
+            default:
+                printf("Wrong use of argument!");
+                break;
+            }
+        
+    }
+    else if( argc > 2 ) {
+        printf("Too many arguments supplied.\n");
+        return 0;
+    }
+    else {
+        printf("One argument expected.\n");
+        return 0;
+    }
 
     init();
     menu(dificulty);
     endwin();
+    attroff(COLOR_PAIR(1));
     return 0;
 }
 void prvy_level(int dificulty)
@@ -27,36 +53,23 @@ void prvy_level(int dificulty)
     char ch;
     FILE *maze1 = fopen("maze1.txt", "r");
     refresh();
-    char maze[84][30];
+    char maze[30][85];
     int i=0; int s = 0;
 
-
-    while(ch!=EOF)
-    {
-        ch = fgetc(maze1);
-        printw("%c", ch);
-        maze[i][s] = ch;
-        if(s==83){
-            i+=1;
-            s=0;
+    for(i=0;i<30;i++){
+        for(s=0;s<85;s++){
+            ch = fgetc(maze1);
+            printw("%c", ch);
+            maze[i][s] = ch;
         }
-        s++;
     }
     endwin();
-
-    for(i=0;i<84;i++){
-        for(s=0;s<30;s++){
-            printf("%c", maze[i][s]);
-        }
-        printf("\n");
-    }
-
     refresh();
 
-    int y = 1; int x = 1; int prev_y = y; int prev_x = x; int X_direction = 0; int Y_direction = 0; int win = 0;
+    int y = 8; int x = 18; int prev_y = y; int prev_x = x; int X_direction = 0; int Y_direction = 0; int win = 0;
     
     while(1){
-        int ch = getch(); int i;
+        int ch = getch(); 
         switch (ch) {
             case KEY_UP: Y_direction = -1; X_direction = 0; break;  
             case KEY_DOWN: Y_direction = 1; X_direction = 0; break;
@@ -75,20 +88,21 @@ void prvy_level(int dificulty)
         printw("X");
         refresh();
 
-        if((y <= 0) || (x<= 0) || (y >= 30) || (x>= 83)){
+        if(maze[y][x] == '*' || maze[y][x] == '#'){
             break;
-        }        
-        /*
-        if(maze[x][y] != ' '){
+        }   
+        if(maze[y][x] == 'O'){
+            win = 1;
             break;
         }      
-        */
     }
 
-    if(win == 0)
-    {
+    if(win == 0){
         clear();
         si_skoncil();
+    }else{
+        clear();
+        si_vyhral();
     }
 
     fclose(maze1);
@@ -102,13 +116,16 @@ void init()
     keypad(stdscr,TRUE);
     curs_set(FALSE);
     nodelay(stdscr, TRUE);
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    attron(COLOR_PAIR(1));
 }
 
 void menu(int dificulty)
 {
     // prints sth to screen (y,x)
     move(LINES/2, COLS/2);
-    printw("PAVOR 2.0");
+    printw("Scary ZAP maze");
     move((LINES/2)+1, COLS/2);
     printw("Press Enter to start!");
 
@@ -124,10 +141,6 @@ void menu(int dificulty)
 
 void si_skoncil()
 {
-    start_color();
-    init_pair(1, COLOR_CYAN, COLOR_WHITE);
-    attron(COLOR_PAIR(1));
-
     clear();
     char ch;
     FILE *koncis = fopen("si_skoncil60.txt", "r");
@@ -153,8 +166,23 @@ void si_skoncil()
     while(c!='\n'){
         c = getch();
     }
-
-    attroff(COLOR_PAIR(1));
-
     fclose(koncis);
+}
+
+void si_vyhral()
+{
+    char ch;
+    clear();
+    refresh();
+
+    move(LINES/2, COLS/2);
+    printw("SI VYHRAL!");
+    move((LINES/2)+1, (COLS/2));
+    printw("Press Enter");
+    refresh();
+
+    int c = 0;
+    while(c!='\n'){
+        c = getch();
+    }
 }
